d24cdebc020628f9d7951b96c95aef2eb3e580da
Luis Antonio Parrado <luisprmat@gmail.com>
2021-08-23 18:48:54 -0500

    Create models and migrations

---
+++ database/migrations/2021_08_23_232703_create_posts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('slug');
+
+            $table->text('extract');
+            $table->longText('body');
+
+            $table->enum('status', [Post::DRAFT, Post::PUBLISHED])->default(Post::DRAFT);
+
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
