a160f1714c2400712e041ad1d92ba3d449de62e3
Luis Antonio Parrado <luisprmat@gmail.com>
2021-08-23 22:08:29 -0500

    Add filters to categories endpoint

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,6 +13,7 @@ class Category extends Model
     protected $fillable = ['name', 'slug'];
 
     protected $allowIncluded = ['posts', 'posts.user'];
+    protected $allowFilter = ['id', 'name', 'slug'];
 
     /** RELATIONSHIPS */
     // One To Many
@@ -38,5 +39,21 @@ class Category extends Model
         }
 
         $query->with($relations);
+    }
+
+    public function scopeFilter(Builder $query)
+    {
+        if (empty($this->allowFilter) || empty(request('filter'))) {
+            return;
+        }
+
+        $filters = request('filter');
+        $allowFilter = collect($this->allowFilter);
+
+        foreach ($filters as $filter => $value) {
+            if ($allowFilter->contains($filter)) {
+                $query->where($filter, 'LIKE', "%{$value}%");
+            }
+        }
     }
 }
