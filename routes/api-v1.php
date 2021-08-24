1974d68502e545183762f56e4a787ee45ba29f9a
Luis Antonio Parrado <luisprmat@gmail.com>
2021-08-24 11:15:25 -0500

    Create endpoint for login

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\Api\CategoryController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\RegisterController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\Auth\LoginController;
 
 
+Route::post('login', [LoginController::class, 'store']);
 Route::post('register', [RegisterController::class, 'store'])->name('register');
 
 Route::apiResource('categories', CategoryController::class)->names('categories');
