74aa68056562cecd1aa9f27d8f48334544908ffd
Luis Antonio Parrado <luisprmat@gmail.com>
2021-08-23 19:53:37 -0500

    Fill database with fake data + images

--- database/migrations/2021_08_23_232703_create_posts_table.php
+++ database/migrations/2021_08_23_232703_create_posts_table.php
@@ -18,7 +18,7 @@ class CreatePostsTable extends Migration
             $table->id();
 
             $table->string('name');
-            $table->string('slug');
+            $table->string('slug')->unique();
 
             $table->text('extract');
             $table->longText('body');
--- database/migrations/2021_08_23_233703_create_tags_table.php
+++ database/migrations/2021_08_23_233703_create_tags_table.php
@@ -17,7 +17,7 @@ class CreateTagsTable extends Migration
             $table->id();
 
             $table->string('name');
-            $table->string('slug');
+            $table->string('slug')->unique();
 
             $table->timestamps();
         });
