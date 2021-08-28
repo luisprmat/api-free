75797503f5a43403d475cd9f023af92ed9ecd98e
Luis Antonio Parrado <luisprmat@gmail.com>
2021-08-28 02:43:32 -0500

    Show clients list

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <x-container id="app" class="py-8">
-        <x-form-section>
+        <x-form-section class="mb-12">
             <x-slot name="title">
                 Crea un nuevo cliente
             </x-slot>
@@ -39,6 +39,41 @@
                 </x-button>
             </x-slot>
         </x-form-section>
+
+        <x-form-section>
+            <x-slot name="title">
+                Lista de clientes
+            </x-slot>
+
+            <x-slot name="description">
+                Aquí podrás encontrar todos lod clientes que has agregado
+            </x-slot>
+
+            <div>
+                <table class="text-gray-600">
+                    <thead class="border-b border-gray-300">
+                        <tr class="text-left">
+                            <th class="py-2 w-full">Nombre</th>
+                            <th class="py-2">Acción</th>
+                        </tr>
+                    </thead>
+
+                    <tbody class="divide-y divide-gray-300">
+                        <tr v-for="client in clients">
+                            <td class="py-2">@{{ client.name }}</td>
+                            <td class="flex divide-x divide-gray-300 py-2">
+                                <a class="pr-2 hover:text-blue-600 font-semibold cursor-pointer">
+                                    Editar
+                                </a>
+                                <a class="pl-2 hover:text-red-600 font-semibold cursor-pointer">
+                                    Eliminar
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </x-form-section>
     </x-container>
 
     @push('js')
@@ -46,13 +81,29 @@
             new Vue({
                 el: '#app',
                 data: {
+                    clients: [],
                     createForm: {
                         errors: [],
                         name: null,
                         redirect: null
                     }
                 },
+                mounted() {
+                    this.getClients()
+                },
                 methods: {
+                    async getClients() {
+                        try {
+                            let response = await axios.get('/oauth/clients')
+                            this.clients = response.data
+                        } catch (e) {
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Oops...',
+                                text: 'No pudimos obtener los clientes, intente nuevamente',
+                            })
+                        }
+                    },
                     store() {
                         axios.post('/oauth/clients', this.createForm)
                             .then(res => {
