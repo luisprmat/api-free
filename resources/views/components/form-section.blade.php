cede35bd40fcc1a2b9f140db126d908304b512f3
Luis Antonio Parrado <luisprmat@gmail.com>
2021-08-28 01:17:56 -0500

    Make form responsive

--- resources/views/components/form-section.blade.php
+++ resources/views/components/form-section.blade.php
@@ -1,5 +1,5 @@
-<div class="grid grid-cols-3 gap-6">
-    <div>
+<div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6']) }}>
+    <div class="px-4 sm:px-0">
         <h3 class="text-lg font-medium text-gray-900">
             {{ $title }}
         </h3>
@@ -9,9 +9,9 @@
         </p>
     </div>
 
-    <div class="col-span-2">
+    <div class="mt-5 md:mt-0 md:col-span-2">
         <div>
-            <div class="p-6 bg-white shadow rounded-tl-md rounded-tr-md">
+            <div class="px-4 py-5 sm:p-6 bg-white shadow rounded-tl-md rounded-tr-md">
                 {{ $slot }}
             </div>
 
